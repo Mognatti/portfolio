fb4e339757020ede1a132298dedf98fd914915b6
Caio Mognatti <caiocsm97@gmail.com>
2023-05-05 17:39:49 -0300

    Changed some visuals and updated general content

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,11 +1,34 @@
 import styles from "./Main.module.scss";
 import classNames from "classnames";
+import photo from "../../assets/foto_pessoal.jpg";
+import { IoLogoGithub, IoLogoLinkedin } from "react-icons/io";
 
 interface Props {
   mode: string;
 }
 
 export default function Main({ mode }: Props) {
+  const contatos = [
+    <a href="https://github.com/Mognatti" target="_blank">
+      <IoLogoGithub
+        size="30"
+        className={classNames({
+          [styles.git]: true,
+          [styles["git__dark"]]: mode === "dark",
+        })}
+      />
+    </a>,
+    <a target="_blank" href="https://www.linkedin.com/in/caio-mognatti/">
+      <IoLogoLinkedin
+        size="30"
+        className={classNames({
+          [styles.linkedin]: true,
+          [styles["linkedin__dark"]]: mode === "dark",
+        })}
+      />
+    </a>,
+  ];
+
   return (
     <section className={styles.main}>
       <div className={styles.intro}>
@@ -15,16 +38,38 @@ export default function Main({ mode }: Props) {
             [styles["intro__title__dark"]]: mode === "dark",
           })}
         ></p>
-        <p className={styles.intro__description}>
-          Me chamo Caio Mognatti, nasci em 1997, e comecei no mundo da promação
-          em 2022. Sempre tive interesse na área de tecnologia, mas acabei
-          seguindo por um caminho diferente durante um bom tempo, até que eu
-          finalmente tomei a decisão de estudar programação. Sinto que encontrei
-          minha vocação e estou muito feliz com a possiblidade de trabalhar com
-          aquilo que realmente gosto. Ainda tenho muito a aprender, mas não me
-          falta determinação para tal! Estudo diariamente e desejo me
-          diferenciar cada vez mais.
-        </p>
+      </div>
+      <div className={styles.about}>
+        <img src={photo} className={styles.about__photo} />
+        <div
+          className={classNames({
+            [styles.about__description]: true,
+            [styles["about__description__dark"]]: mode === "dark",
+          })}
+        >
+          <p>
+            Desenvolvedor Web Full Stack, comecei a estudar programação em 2022.
+            Sou formado em biomedicina e já cheguei a lecionar na área, mas
+            optei por trocar de carreira assim que conheci mais afundo as
+            tarefas de um dev. Sempre tive interesse na área de tecnologia e,
+            quando resolvi estudar, me apaixonei. Sinto que encontrei minha
+            vocação e estou muito feliz com a possibilidade de trabalhar com
+            aquilo que realmente gosto. Ainda tenho muito a aprender, mas não me
+            falta determinação para tal! Estudo diariamente e desejo me
+            diferenciar cada vez mais.
+          </p>
+        </div>
+        <div className={styles.about__description__icons}>
+          {contatos.map((icon) => (
+            <span
+              className={classNames({
+                [styles.about__description__icon]: true,
+              })}
+            >
+              {icon}
+            </span>
+          ))}
+        </div>
       </div>
     </section>
   );
