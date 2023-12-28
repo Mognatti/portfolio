b36f8154618e734eddbfe344c74593e00910e0b8
Caio Mognatti <caiocsm97@gmail.com>
2023-12-28 19:06:18 -0300

    feat: small change on tech stack order of display

--- src/pages/main/Tecnologias/tecnologias.tsx
+++ src/pages/main/Tecnologias/tecnologias.tsx
@@ -19,13 +19,6 @@ export const tecnologias = [
     link: "https://developer.mozilla.org/en-US/docs/Web/CSS",
   },
   {
-    nome: "Sass",
-    icon: <SiSass size="40" />,
-    description:
-      "Utilizo essa extensão do CSS em conjunto com o CSS modules para melhor estilizar e organizar as minhas aplicações. Além dele, ainda tenho conhecimento e prática com Styled Components.",
-    link: "https://sass-lang.com/",
-  },
-  {
     nome: "JavaScript",
     icon: <SiJavascript size="40" />,
     description:
@@ -39,6 +32,14 @@ export const tecnologias = [
       "Tenho domínio sobre o uso de TypeScript aplicado ao React, sendo capaz de tornar o código mais organizado e menos suscetível a erros durante o seu desenvolvimento",
     link: "https://www.typescriptlang.org/docs/handbook/intro.html",
   },
+
+  {
+    nome: "Sass",
+    icon: <SiSass size="40" />,
+    description:
+      "Utilizo essa extensão do CSS em conjunto com o CSS modules para melhor estilizar e organizar as minhas aplicações. Além dele, ainda tenho conhecimento e prática com Styled Components.",
+    link: "https://sass-lang.com/",
+  },
   {
     nome: "React",
     icon: <FaReact size="40" />,
@@ -46,13 +47,7 @@ export const tecnologias = [
       "Principal ferramenta que utilizo atualmente. Sou capaz de realizar testes em Jest, tenho conhecimento sobre diversas bibliotecas e posso aprender novas com facilidade.",
     link: "https://react.dev/learn",
   },
-  {
-    nome: "GitHub",
-    icon: <IoLogoGithub size="40" />,
-    description:
-      "Utilizo o GitHub como ferramenta de controle de versão. Sou capaz de trabalhar com a equipe para transformar nossos 'commits' em 'pushes' e 'merges' :) ",
-    link: "https://docs.github.com/pt",
-  },
+
   {
     nome: "NodeJs",
     icon: <FaNodeJs size="40" />,
@@ -63,8 +58,14 @@ export const tecnologias = [
   {
     nome: "Firebase",
     icon: <SiFirebase size="40" />,
-    description:
-      "Ferramenta que utilizo atualmente para lidar com autenticação de usuários e/ou banco de dados.",
+    description: "Ferramenta que utilizo atualmente para lidar com autenticação de usuários e/ou banco de dados.",
     link: "https://firebase.google.com/docs/guides?hl=pt-br",
+  },
+  {
+    nome: "GitHub",
+    icon: <IoLogoGithub size="40" />,
+    description:
+      "Utilizo o GitHub como ferramenta de controle de versão. Sou capaz de trabalhar com a equipe para transformar nossos 'commits' em 'pushes' e 'merges'",
+    link: "https://docs.github.com/pt",
   },
 ];
